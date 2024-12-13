b4efd08deaa8e97ac0189fdd17050b24191d6723
ANGELMUGIWARA <20233tn144@utez.edu.mx>
2024-12-13 12:37:17 -0600

    Cambios

--- InkBloom/src/main/java/com/example/InkBloom/Controller/DocumentoController.java
+++ InkBloom/src/main/java/com/example/InkBloom/Controller/DocumentoController.java
@@ -26,45 +26,39 @@ public class DocumentoController {
      * @param model El objeto Model permite enviar datos al archivo HTML.
      * @return El nombre del archivo HTML que se renderizará (sin extensión).
      */
-    @GetMapping
-    public String listarDocumentos(Model model) {
-        List<Documento> documentos = documentoService.listarDocumentos();
-        model.addAttribute("documentos", documentos);
-        return "documento";
-    }
+
 
     @GetMapping("/crear")
     public String crearDocumento(@RequestParam String titulo, Model model) {
         documentoService.crearDocumento(titulo); // Crea el documento
-        model.addAttribute("documento", documentoService.obtenerDocumento()); // Agrega el documento al modelo
+
         return "documento"; // Devuelve la vista "documento"
     }
 
     @PostMapping("/agregar")
     public String agregarTexto(@RequestParam String texto, Model model) {
         documentoService.agregarTexto(texto);
-        model.addAttribute("documento", documentoService.obtenerDocumento());
+
         return "documento";
     }
 
     @PostMapping("/quitar")
     public String quitarTexto(Model model) {
         documentoService.quitarTexto();
-        model.addAttribute("documento", documentoService.obtenerDocumento());
+
         return "documento";
     }
 
     @PostMapping("/deshacer")
     public String deshacer(Model model) {
         documentoService.deshacer();
-        model.addAttribute("documento", documentoService.obtenerDocumento());
+
         return "documento";
     }
 
     @PostMapping("/rehacer")
     public String rehacer(Model model) {
         documentoService.rehacer();
-        model.addAttribute("documento", documentoService.obtenerDocumento());
         return "documento";
     }
 
